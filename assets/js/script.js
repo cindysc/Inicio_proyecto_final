26a8947b441cd5c792bf7fd08adb324180ed8fe5
Cindy <cindy.sanhueza2011@gmail.com>
2018-11-30 23:57:06 -0300

    js

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+/* Initialize Swiper */
+$(document).ready(function(){
+    /* Initialize Swiper */
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+    /* filtro gris galería */
+    $('img').on('mouseover', function(){
+        $(this).toggleClass('filtro');
+    });
+
+    /* alert inscripción terminada */
+    $(".modal-footer").on ("click", '.clase-button', function(){
+        alert("Gracias por su inscripción, nos contactaremos contigo a la brevedad");
+    });
+});
