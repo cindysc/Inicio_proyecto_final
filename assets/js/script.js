e0bbe177759c5e8ed12db1ee1c0e51dee90e19f1
Cindy <cindy.sanhueza2011@gmail.com>
2018-12-02 02:21:50 -0300

    js ajustes

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,13 +15,53 @@ $(document).ready(function(){
       },
     });
 
+    $('.card .footer:first').append('<p class="tag_add"> ¡Últimos cupos! </p>'); //index card - append
+    $('.card .title:last').prepend('<p class="tag">Nuevo: </p> ');  //index card -  preppend
+
+    /* Noticias - ver más noticias  */
+    $('#select_mas').click(function(e){
+        e.preventDefault();
+        $('.articulo').removeClass('hidden');
+        $(this).css('opacity', '0.5');
+        $(this).removeClass('mas_art');
+    });
+
     /* filtro gris galería */
     $('.example-image').on('mouseover', function(){
         $(this).toggleClass('filtro');
     });
 
     /* alert inscripción terminada */
-    $(".modal-footer").on ("click", '.clase-button', function(){
+    $('.modal-footer').on ('click', '.clase-button', function(){
         alert("Gracias por su inscripción, nos contactaremos contigo a la brevedad");
+        $('#exampleModal-final').removeClass('hidden');
+        $('#exampleModal').addClass('hidden');
+
+
     });
+
+
+    $('#cargar_mas').click (function(){
+        console.log('Gracias por su inscripción, nos contactaremos contigo a la brevedad');
+        $('#mas_cursos').removeClass('hidden');
+    });
+
+
+
+
+
+
+    /* .card p:first
+    $('.footer').on('click', 'button', function(){
+        event.stopPropagation();
+        $(this).parent().fadeOut(500);
+    });
+
+    $(document).on('click','#contact_tools li a',function(){
+
+        $('#contact_tools li').removeClass('active');
+        $(this).parent('li').addClass('active');
+
+    });*/
+
 });
